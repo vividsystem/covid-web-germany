4ec8ed9ed24b804479cee308e27510c87141b807
vividsystem <notrealbutnotobot@gmail.com>
2021-02-26 13:46:46 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Corona API for Germany
+## This is a MVP and not ment for production deployment use!
 This project is a Corona Website/API for Germany using the RKI(Robert Koch Institut) as Source.
+
 # Usage
 python3 main.py
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
 from flask import Flask, render_template, redirect
 from website import create_app
-
-app = create_app()
-
+from website.models import Entry
+from website.scraper import get_corona_info, update_db
+from datetime import datetime
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+  app = create_app()
+  app.run(host="0.0.0.0", debug=False)
